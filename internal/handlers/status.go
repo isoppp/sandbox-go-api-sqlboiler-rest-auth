7a8d52b5190aed61a7839feaca16084f4f4a38d0
isoppp <prog@d2deck.com>
2021-09-20 11:06:19 +0900

    add middleware which sets user from session

--- internal/handlers/status.go
+++ internal/handlers/status.go
@@ -1,11 +1,26 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"sandbox-go-api-sqlboiler-rest-auth/models"
 
 	"github.com/labstack/echo/v4"
 )
 
+type CookieValue struct {
+	UserID int
+	Name   string
+}
+
 func (h *Handlers) GetStatus(c echo.Context) error {
+	var u *models.User
+	uv := c.Get("user")
+	if uv != nil {
+		u = uv.(*models.User)
+		fmt.Println("user data?", u)
+	} else {
+		fmt.Println("not set user session")
+	}
 	return c.String(http.StatusOK, "server is running")
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -17,7 +17,7 @@ import (
 func NewRouter(db *sql.DB, l *zap.Logger) *echo.Echo {
 	h := handlers.NewHandler(db, l)
 	e := echo.New()
-	bindRouteMiddlewares(e, l)
+	bindRouteMiddlewares(e, l, db)
 
 	// routes
 	e.GET("/api/status", h.GetStatus)
@@ -40,13 +40,14 @@ func bindRoutes(e *echo.Echo, h *handlers.Handlers) {
 	e.DELETE("/api/v1/users/:id", h.DeleteUser)
 }
 
-func bindRouteMiddlewares(e *echo.Echo, logger *zap.Logger) {
+func bindRouteMiddlewares(e *echo.Echo, logger *zap.Logger, db *sql.DB) {
 	// middlewares
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(ZapLogger(logger))
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{}))
 	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{}))
+	e.Use(SessionRestorer(db))
 
 	// middlewares if production
 	//e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
